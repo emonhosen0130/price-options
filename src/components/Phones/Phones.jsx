bbd39a4b04b4fa0eadaa1cbe1923510e0244e527
emonhosen0130 <irfanmuhammad013090@gmail.com>
2024-03-31 07:10:58 +0600

    finish the work

--- src/components/Phones/Phones.jsx
+++ src/components/Phones/Phones.jsx
@@ -1,8 +1,11 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { Audio } from 'react-loader-spinner';
+
 const Phones = () => {
-    const [Phones, setPhones] = useState([])
+    const [Phones, setPhones] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         // fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
@@ -12,26 +15,38 @@ const Phones = () => {
         axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
             .then(data => {
                 const phoneData = data.data.data;
-                const phonesWithFakeData = phoneData.map(phone =>{
-                    const obj ={
+                const phonesWithFakeData = phoneData.map(phone => {
+                    const obj = {
                         name: phone.phone_name,
-                        price:parseInt(phone.slug.split('-')[1])
+                        price: parseInt(phone.slug.split('-')[1])
                     }
                     return obj;
                 })
                 console.log(phonesWithFakeData);
-                setPhones(phonesWithFakeData)
+                setPhones(phonesWithFakeData);
+                setLoading(false)
             })
     }, [])
     return (
         <div>
+            {
+              loading &&  <Audio
+                    height="80"
+                    width="80"
+                    radius="9"
+                    color="green"
+                    ariaLabel="three-dots-loading"
+                    wrapperStyle
+                    wrapperClass
+                />
+            }
             <h2 className="text-4xl">Phones:{Phones.length}</h2>
             <BarChart width={1200} height={400} data={Phones}>
-          <Bar dataKey="price" fill="#8884d8" />
-          <XAxis dataKey='name'></XAxis>
-          <YAxis></YAxis>
-          <Tooltip></Tooltip>
-        </BarChart>
+                <Bar dataKey="price" fill="#8884d8" />
+                <XAxis dataKey='name'></XAxis>
+                <YAxis></YAxis>
+                <Tooltip></Tooltip>
+            </BarChart>
         </div>
     );
 };
